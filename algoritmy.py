1f4fd69d0724525db29cfbc8cb5bbfdd5cd21f17
polozyuk <polozyuke@gmail.com>
2020-12-16 19:16:00 +0200

    Add algoritmy file

---
+++ algoritmy.py
@@ -0,0 +1,27 @@
+def find_Smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = find_Smallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i < pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+print(quicksort([10, 5, 2, 3]))
+#print(selectionSort([5, 3, 6, 2, 10]))
