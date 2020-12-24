35c2c35a0736af5bf6c960227903071aa0b0e4fb
polozyuk <polozyuke@gmail.com>
2020-12-24 18:24:05 +0200

    update1224

--- algoritmy.py
+++ algoritmy.py
@@ -26,4 +26,48 @@ def quicksort(array):
 #print(quicksort([10, 5, 2, 3]))
 #print(selectionSort([5, 3, 6, 2, 10]))
 
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+    
+graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+graph["a"] = {}
+graph["a"]["fin"] = 1
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+graph["fin"] = {}
+infinity = float("inf")
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+processed = []
 
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
+
+
+print(graph)
